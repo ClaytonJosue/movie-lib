4b5a433460066ecb3b34932a378df9144153f42f
ClaytonJosue <jogamesbr123@gmail.com>
2023-01-11 15:21:50 -0300

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from './components/navbar/Navbar'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Movie Lib</h2>
+      <Outlet />
     </div>
   )
 }
