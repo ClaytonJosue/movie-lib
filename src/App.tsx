c3e3c1e79e9afb04cccfba50bb4059ff40fc9a85
ClaytonJosue <jogamesbr123@gmail.com>
2023-01-12 18:00:30 -0300

    created the movies card

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Movie Lib</h2>
       <Outlet />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,32 @@
+import MovieCard from "../components/movies/MovieCard";
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+	const [topMovies, setTopMovies] = useState([]);
+
+	const getTopRatedMovies = async (url: string) => {
+		const res = await fetch(url)
+		const data = await res.json()
+
+		setTopMovies(data.results);
+	}
+
+	useEffect(() => {
+		const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+		getTopRatedMovies(topRatedURL);
+	}, [])
+
 	return (
-		<div>Home</div>
+		<div className="container">
+			<h2 className="title">Melhores Filmes:</h2>
+			<div className="movies-container">
+				{topMovies.length === 0 && <p>Carregando...</p>}
+				{topMovies.length > 0 && topMovies.map((movie: any) => <MovieCard key={movie.id} movie={movie} />)}
+			</div>
+		</div>
 	)
 }
 
